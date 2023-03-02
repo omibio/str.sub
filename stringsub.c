b0d4e857d81e5e96bf75acb20d40f88b06b347b9
OMIBIO <84881662+omibio@users.noreply.github.com>
2023-03-02 01:21:59 -0300

    Update stringsub.c

--- stringsub.c
+++ stringsub.c
@@ -2,27 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef int I;
-
-extern char str[55] = "teste";
-I main() {
-	strsub(str, 1, 1);
-	printf("%s", str);
-	return 0;
-}
-
-void strsub(char strr[], I ed, I de) {
-  I strt = strlen(strr);
-  I a; I b = -1;
+void strsub(char strr[], int ed, int de) {
+  int strt = strlen(strr);
+  int a; int b = 0;
   if (ed) {
     for (a = ed; a <= strt; a++) {
-      b++;
       strr[b] = strr[a];
+      b++;
     }
   }
-  if (de) {
-    strt = strlen(strr);
-    I rde = strt - de;
-    strr[rde] = '\0';
-  }
+  strt = strlen(strr);
+  int rde = strt - de;
+  strr[rde] = '\0';
+}
+
+int main() {
+  char str[55] = "teste";
+	strsub(str, 2, 1);
+	printf("%s", str);
+	return 0;
 }
